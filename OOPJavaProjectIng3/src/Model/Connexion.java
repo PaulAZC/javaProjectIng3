c33f729010bc895446a493c6aa1ba878675e8a6d
PaulAZC <ayzacp@gmail.com>
2020-11-23 22:31:33 +0100

    Etablissement de la connexion
    Error : Column 'Username' not found

---
+++ OOPJavaProjectIng3/src/Model/Connexion.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.sql.*;
+/**
+ *
+ * @author ayzac
+ */
+public class Connexion {
+    private final String URL = "jdbc:mysql://localhost:3308/medical_application";
+    private String sqlStatement;
+    private int id=0;
+
+    public int verifConnection(String username, String password)
+    {
+        try
+        {            
+            int b=0;
+            Connection co = DriverManager.getConnection(URL, "root", "paul1234");
+            Statement stmt = co.createStatement();
+            sqlStatement = "SELECT Id FROM connexion WHERE Username='" +username+ "' && Password='"+password+"';";
+            ResultSet result = stmt.executeQuery(sqlStatement);
+            while(result.next() && b==0)
+            {
+                if(result.getString("Username")==username && result.getString("Password")==password)
+                {
+                    id = result.getInt("Id");
+                    b=1;
+                }
+            }
+        }
+        catch(SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        return id;
+    }
+}
